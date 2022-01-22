3c932702bd88af8617ceba63b51d036e9c47a546
Ryan <leeyuenhigh@gmail.com>
2022-01-21 20:31:55 -0500

    Created user controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+const getUsers = (request, response) => {
+  console.log(process.env.DB_PORT);
+  pool.query("SELECT * FROM users ORDER BY id", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const deleteUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  // createUser,
+  // updateUser,
+  // deleteUserById
+};
